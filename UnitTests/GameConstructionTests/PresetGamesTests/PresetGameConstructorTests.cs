f55d87c4238df1899ae4bd78158a68f8c696b929
Joshua Jacobs <joshua.jacobs126@gmail.com>
2022-06-26 17:23:30 -0500

    Created PresetGameConstructor and tests

---
+++ UnitTests/GameConstructionTests/PresetGamesTests/PresetGameConstructorTests.cs
@@ -0,0 +1,27 @@
+﻿using deckForge.GameConstruction.PresetGames;
+using FluentAssertions;
+
+namespace UnitTests.GameConstructionTests.PresetGamesTests
+{
+    [TestClass]
+    public class PresetGameConstructorTests {
+
+        [TestMethod]
+        public void PassInvalidGameName() {
+            string name = "ThisIsAFakeGameName";
+ 
+            Action a = () => new PresetGameConstructor(name);
+            a.Should().Throw<ArgumentException>($"because there is no game called {name}");
+        }
+
+        [TestMethod]
+        [DataRow("war")]
+        [DataRow("WAR")]
+        [DataRow("wAr")]
+        public void PassValidGameName_WithVariousCasing(string name) {
+            PresetGameConstructor pgc = new PresetGameConstructor(name);
+
+            pgc.isValidGame().Should().Be(true, "the game should be able to be found and be case insensitive");
+        }
+    }
+}
---
+++ deckForge/GameConstruction/PresetGames/PresetGameConstructor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+
+namespace deckForge.GameConstruction.PresetGames
+{
+    public class PresetGameConstructor
+    {
+        private bool _validGame = false;
+        public PresetGameConstructor(string gameName) {
+            if (PresetGameValidator.isPresetGameMade(gameName.ToUpper()) > -1)
+            {
+                _validGame = true;
+            }
+            else {
+                throw new ArgumentException("Premade Game name not found");
+            }
+        }
+
+        public bool isValidGame() {
+            return _validGame;
+        }
+
+        //This class only makes sure that there is a preset game matching the string passed
+        private static class PresetGameValidator {
+            static Hashtable presetGames = new Hashtable() {
+                { "WAR", 0 }
+            };
+
+            //-1 if game DNE otherwise returns ID
+            static public int isPresetGameMade(string s) {
+                if (presetGames.ContainsKey(s))
+                {
+                    return (int)presetGames[s]!;
+                }
+                else
+                    return -1;
+            }
+            
+        }
+    }
+}
