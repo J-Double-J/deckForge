bee8842755ad8db5ec7656daa43f24ea5421ff49
Joshua Jacobs <joshua.jacobs126@gmail.com>
2022-06-26 11:14:34 -0500

    Basic SetUpRules created

--- UnitTests/GameRulesTests/SetUpRulesTests.cs
+++ UnitTests/GameRulesTests/SetUpRulesTests.cs
@@ -1,11 +1,38 @@
 ﻿using DeckNameSpace;
 using FluentAssertions;
+using deckForge.GameRules;
+using GameNamespace;
+using PlayerNamespace;
 
 namespace UnitTests.GameRulesTests
 {
     [TestClass]
     public class SetUpRulesTests
     {
+        [TestMethod]
+        [DataRow(1)]
+        [DataRow(3)]
+        public void CreateNumerousDeck(int count) {
+            SetUpRules spr = new SetUpRules(deckCount: count);
 
+            spr.Decks.Count().Should().Be(count, "SetUpRules was told to create numerous decks");
+        }
+
+        [TestMethod]
+        [DataRow(-1)]
+        [DataRow(0)]
+        public void AttemptToCreateNegativeOrNilDecks(int count) {
+            Action act = () => new SetUpRules(deckCount: count);
+            act.Should().Throw<ArgumentException>("SetUpRules cannot make a game with no decks");
+        }
+
+        [TestMethod]
+        public void PlayerShouldHave_CorrectInitHandSize() {
+            GameMediator gm = new(0);
+            SetUpRules spr = new SetUpRules(initHandSize: 8);
+            Player p = new(gm, spr.GetHashCode());
+
+            p.HandSize().Should().Be(8, "SetUpRules set the initial hand size to 8");
+        } 
     }
 }
--- deckForge/GameRules/SetUpRules.cs
+++ deckForge/GameRules/SetUpRules.cs
@@ -1,7 +1,50 @@
-﻿namespace deckForge.GameRules
+﻿using DeckNameSpace;
+
+namespace deckForge.GameRules
 {
     public class SetUpRules
     {
+        private int _deckCount;
+        private int _initHandSize;
+        public readonly List<Deck> Decks = new List<Deck>();
 
+        public SetUpRules(int deckCount = 1, int initHandSize = 0) {
+            try
+            {
+                DeckCount = deckCount;
+                InitHandSize = initHandSize;
+            }
+            catch {
+                throw;
+            }
+            
+        }
+
+        public int DeckCount { 
+            get { return _deckCount; } 
+            private set {
+                if (value > 0)
+                {
+                    _deckCount = value;
+                    for (var i = 0; i < value; i++)
+                    {
+                        Decks.Add(new Deck()); //TODO use better logic to create the right decks and their types
+                    }
+                }
+                else {
+                    throw new ArgumentException("Cannot initialize a game and have a deck count that is 0 or less");
+                }
+            } 
+        }
+
+        public int InitHandSize {
+            get { return _initHandSize; }
+            private set {
+                if (value >= 0)
+                    _initHandSize = value;
+                else
+                    throw new ArgumentException("Cannot have a negative hand size");
+            }        
+        }
     }
 }
