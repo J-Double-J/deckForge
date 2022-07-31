e9a5e777bcf2c4996c917ec718447a2a1cceae35
Joshua Jacobs <joshua.jacobs126@gmail.com>
2022-07-31 16:38:22 -0500

    Updated BaseSetUpRules and InterPlayerRoundRules to StyleCop Standards

---
+++ deckForge/GameRules/BaseSetUpRules.cs
@@ -0,0 +1,93 @@
+﻿using DeckForge.GameElements.Resources;
+
+namespace DeckForge.GameRules
+{
+    /// <summary>
+    /// Base class for any set up rules that a game must concern itself before it begins playing.
+    /// </summary>
+    public class BaseSetUpRules
+    {
+        private int deckCount;
+        private int initHandSize;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BaseSetUpRules"/> class.
+        /// </summary>
+        /// <param name="deckCount">Number of decks to create on set up.</param>
+        /// <param name="initHandSize">Initial hand size of <see cref="PlayerConstruction.IPlayer"/>s at the start
+        /// of the game.</param>
+        public BaseSetUpRules(int deckCount = 1, int initHandSize = 0)
+        {
+            Decks = new ();
+            try
+            {
+                DeckCount = deckCount;
+                InitHandSize = initHandSize;
+            }
+            catch
+            {
+                throw;
+            }
+
+            foreach (Deck d in Decks)
+            {
+                d.Shuffle();
+            }
+        }
+
+        /// <summary>
+        /// Gets the list of Decks that <see cref="BaseSetUpRules"/> has.
+        /// </summary>
+        public List<Deck> Decks { get; }
+
+        /// <summary>
+        /// Gets the number of decks that are managed by <see cref="BaseSetUpRules"/>.
+        /// </summary>
+        public int DeckCount
+        {
+            get
+            {
+                return deckCount;
+            }
+
+            private set
+            {
+                if (value > 0)
+                {
+                    deckCount = value;
+                    for (var i = 0; i < value; i++)
+                    {
+                        Decks.Add(new Deck()); // TODO: use better logic to create the right decks and their types
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException("Cannot initialize a game and have a deck count that is 0 or less");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the initial hand size of the <see cref="PlayerConstruction.IPlayer"/>s.
+        /// </summary>
+        public int InitHandSize
+        {
+            get
+            {
+                return initHandSize;
+            }
+
+            private set
+            {
+                if (value >= 0)
+                {
+                    initHandSize = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Cannot have a negative hand size");
+                }
+            }
+        }
+    }
+}
--- deckForge/GameRules/InterPlayerRoundRules.cs
+++ deckForge/GameRules/InterPlayerRoundRules.cs
@@ -1,5 +1,10 @@
 ﻿namespace DeckForge.GameRules
 {
+    /// <summary>
+    /// Gets the rules that occur between rounds.
+    /// Optional class that can do more indepth logic handling outside of <see cref="DeckForge.GameConstruction.IGameMediator"/>
+    /// where too much would be inappropriate for the mediator.
+    /// </summary>
     public class InterPlayerRoundRules
     {
     }
