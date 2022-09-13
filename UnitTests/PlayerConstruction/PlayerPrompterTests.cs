439255fd4215a0327873aa72d7a031e3eef38e05
Joshua Jacobs <joshua.jacobs126@gmail.com>
2022-09-12 20:13:55 -0500

    PlayerPrompter is made with Test

---
+++ UnitTests/PlayerConstruction/PlayerPrompterTests.cs
@@ -0,0 +1,40 @@
+﻿using DeckForge.PlayerConstruction;
+using FluentAssertions;
+
+namespace UnitTests.PlayerConstruction
+{
+    [TestClass]
+    public class PlayerPrompterTests
+    {
+        [TestMethod]
+        public void PlayerCanChooseFromPrompt_WithValidChoice() {
+            var stringReader = new StringReader("1");
+            Console.SetIn(stringReader);
+            string prompt = "What number is your favorite?\n" +
+                "1) 1!\n" +
+                "2) 2!\n" +
+                "3) Neither of these.";
+            PlayerPrompter pp = new(prompt, 3);
+
+            pp.Prompt().Should().Be(1, "the Player chose option 1.");
+        }
+
+        [TestMethod]
+        public void PlayerCannotChooseFromPrompt_WithInvalidChoice() {
+            var stringReader = new StringReader("J");
+            Console.SetIn(stringReader);
+            var stringReaderThree = new StringReader("3");
+            Console.SetIn(stringReaderThree);
+            var stringReaderTwo = new StringReader("100");
+            Console.SetIn(stringReaderTwo);
+            
+            string prompt = "What number is your favorite?\n" +
+                "1) 1!\n" +
+                "2) 2!\n" +
+                "3) Neither of these.";
+            PlayerPrompter pp = new(prompt, 3);
+
+            pp.Prompt().Should().Be(3, "the Player chose option 1.");
+        }
+    }
+}
---
+++ deckForge/PlayerConstruction/PlayerPrompter.cs
@@ -0,0 +1,56 @@
+﻿namespace DeckForge.PlayerConstruction
+{
+    public class PlayerPrompter
+    {
+        private string prompt;
+        private int optionCount;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PlayerPrompter"/> class.
+        /// </summary>
+        /// <param name="prompt">The prompt the player will be shown when they need to make a choice. First option
+        /// starts at 1.</param>
+        /// <param name="optionCount">The number of valid options for the <see cref="IPlayer"/> to choose from.</param>
+        public PlayerPrompter(string prompt, int optionCount)
+        {
+            this.prompt = prompt;
+            this.optionCount = optionCount;
+        }
+
+        /// <summary>
+        /// Presents the Player with the given prompt.
+        /// </summary>
+        /// <returns>An integer that is the response that the <see cref="IPlayer"/> chose to execute.</returns>
+        public int Prompt()
+        {
+            string? response;
+            do
+            {
+                Console.WriteLine(prompt);
+                response = Console.ReadLine();
+            }
+            while (!IsValidResponse(response));
+
+            return int.Parse(response!);
+        }
+
+        private bool IsValidResponse(string? response)
+        {
+            if (int.TryParse(response, out int numericResponse))
+            {
+                if (numericResponse > 0 && numericResponse <= optionCount)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
