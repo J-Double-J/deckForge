347d7c4da07b05efdffd41ce5cf8bef626581abf
Joshua Jacobs <joshua.jacobs126@gmail.com>
2022-09-26 21:15:13 -0500

    Created SimplisticHandEvaluator
    Not a real Poker Hand evaluator

---
+++ UnitTests/PokerTests/SimpleEvaluatorTests.cs
@@ -0,0 +1,66 @@
+﻿using DeckForge.GameConstruction.PresetGames.Poker;
+using DeckForge.GameElements;
+using DeckForge.GameElements.Resources;
+using DeckForge.PhaseActions;
+using DeckForge.PhaseActions.PlayerActions;
+using FluentAssertions;
+
+namespace UnitTests.PokerTests
+{
+    [TestClass]
+    public class SimpleEvaluatorTests
+    {
+        [TestMethod]
+        public void EvaluatorGetsExpectedValues()
+        {
+            PokerGameMediator pGM = new PokerGameMediator(2);
+            Table table = new Table(pGM, 2, 1);
+            Dictionary<int, List<PlayingCard>> hands = new();
+            PlayerGameAction playCards = new PlayHandToTable();
+
+            table.AddCardsTo_NeutralZone(
+                new List<PlayingCard>()
+                {
+                    new PlayingCard(2, "Q"), new PlayingCard(3, "Q"), new PlayingCard(4, "Q")
+                },
+                0);
+
+            table.PlayerPlayedCards[0].AddRange(new List<PlayingCard>() { new PlayingCard(10, "J"), new PlayingCard(5, "J") });
+            table.PlayerPlayedCards[1].AddRange(new List<PlayingCard>() { new PlayingCard(1, "J"), new PlayingCard(2, "J") });
+
+            hands.Add(0, pGM.Table!.GetCardsForSpecificPlayer(0).Concat(pGM.Table!.GetCardsForSpecificNeutralZone(0)).ToList());
+            hands.Add(1, pGM.Table!.GetCardsForSpecificPlayer(1).Concat(pGM.Table!.GetCardsForSpecificNeutralZone(0)).ToList());
+
+            List<int> winner = SimplisitcPokerHandEvaluator.EvaluateHands(hands);
+
+            winner.Count.Should().Be(1, "only one player wins here");
+            winner[0].Should().Be(0, "first player had a higher value hand");
+        }
+
+        [TestMethod]
+        public void EvaluatorHandlesTiesCorrectly()
+        {
+            PokerGameMediator pGM = new PokerGameMediator(2);
+            Table table = new Table(pGM, 2, 1);
+            Dictionary<int, List<PlayingCard>> hands = new();
+            PlayerGameAction playCards = new PlayHandToTable();
+
+            table.AddCardsTo_NeutralZone(
+                new List<PlayingCard>()
+                {
+                    new PlayingCard(2, "Q"), new PlayingCard(3, "Q"), new PlayingCard(4, "Q")
+                },
+                0);
+
+            table.PlayerPlayedCards[0].AddRange(new List<PlayingCard>() { new PlayingCard(10, "J"), new PlayingCard(5, "J") });
+            table.PlayerPlayedCards[1].AddRange(new List<PlayingCard>() { new PlayingCard(10, "D"), new PlayingCard(5, "D") });
+
+            hands.Add(0, pGM.Table!.GetCardsForSpecificPlayer(0).Concat(pGM.Table!.GetCardsForSpecificNeutralZone(0)).ToList());
+            hands.Add(1, pGM.Table!.GetCardsForSpecificPlayer(1).Concat(pGM.Table!.GetCardsForSpecificNeutralZone(0)).ToList());
+
+            List<int> winner = SimplisitcPokerHandEvaluator.EvaluateHands(hands);
+
+            winner.Count.Should().Be(2, "both players have the same hand value so there is a tie");
+        }
+    }
+}
---
+++ deckForge/GameConstruction/PresetGames/Poker/SimplisitcPokerHandEvaluator.cs
@@ -0,0 +1,55 @@
+﻿using DeckForge.GameElements.Resources;
+
+namespace DeckForge.GameConstruction.PresetGames.Poker
+{
+    /// <summary>
+    /// A simplistic hand evaluator that does not follow Poker rules but serves as an example/proof of concept
+    /// </summary>
+    public class SimplisitcPokerHandEvaluator
+    {
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SimplisitcPokerHandEvaluator"/> class.
+        /// </summary>
+        public SimplisitcPokerHandEvaluator()
+        {
+        }
+
+        /// <summary>
+        /// Evaluates a dictionary of poker hands and determines a winner based on the summation of their cards in their hand. This is not how Poker
+        /// works, this is a simplistic lazy example.
+        /// </summary>
+        /// <param name="hands">A dictionary with a key value pair of a Player's ID and their hand.</param>
+        /// <returns>A list of Player IDs that are the winners.</returns>
+        public static List<int> EvaluateHands(Dictionary<int, List<PlayingCard>> hands)
+        {
+            int highestHandVal = 0;
+            int currentHandVal = 0;
+            List<int> highestHandKeys = new();
+
+            // TODO: There are great Poker Hand Evaluators out there, but I decided that was outside the scope so I made an impractical simple evaluator
+            // that does not remotely follow normal rules. At some point this may be fixed
+            foreach (var hand in hands)
+            {
+                currentHandVal = 0;
+                for (var i = 0; i < hand.Value.Count; i++)
+                {
+                    currentHandVal += hand.Value[i].Val;
+                }
+
+                if (currentHandVal > highestHandVal)
+                {
+                    highestHandKeys.Clear();
+                    highestHandKeys.Add(hand.Key);
+                    highestHandVal = currentHandVal;
+                }
+                else if (currentHandVal == highestHandVal)
+                {
+                    highestHandKeys.Add(hand.Key);
+                }
+            }
+
+            return highestHandKeys;
+        }
+    }
+}
