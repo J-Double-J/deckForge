99504d15ac57a64b82cfee559dfa5abb4adcbd97
Joshua Jacobs <joshua.jacobs126@gmail.com>
2022-11-19 11:51:58 -0600

    Added new class to handle resource payments

---
+++ UnitTests/HelperObjectTests/CostVerifierTests.cs
@@ -0,0 +1,136 @@
+﻿using DeckForge.GameElements.Resources.Cards;
+using FluentAssertions;
+
+namespace UnitTests.HelperObjectTests
+{
+    [TestClass]
+    public class CostVerifierTests
+    {
+        [TestMethod]
+        public void CostIsPaidForAndVerified()
+        {
+            Dictionary<Type, int> cost = new() { { typeof(Wood), 2 }, { typeof(Coin), 3 } };
+            Dictionary<Type, int> payment = new() { { typeof(Wood), 2 }, { typeof(Coin), 3 } };
+
+            CostVerifier.VerifyPaymentExactly(cost, payment).Should().Be(true, "cost and payment were the same");
+        }
+
+        [TestMethod]
+        public void PaymentExactlyFailsEvenIfMorePaidThanCost()
+        {
+            Dictionary<Type, int> cost = new() { { typeof(Wood), 2 }, { typeof(Coin), 3 } };
+            Dictionary<Type, int> payment = new() { { typeof(Wood), 2 }, { typeof(Coin), 4 } };
+
+            CostVerifier.VerifyPaymentExactly(cost, payment).Should().Be(false, "payment is overpayed for the cost");
+        }
+
+        [TestMethod]
+        public void PaymentFailsIfMinimumNotCovered()
+        {
+            Dictionary<Type, int> cost = new() { { typeof(Wood), 2 }, { typeof(Coin), 3 } };
+            Dictionary<Type, int> payment = new() { { typeof(Wood), 1 }, { typeof(Coin), 3 } };
+
+            CostVerifier.VerifyPaymentExactly(cost, payment).Should().Be(false, "payment does not equal cost");
+        }
+
+        [TestMethod]
+        public void PaymentSucceedsIfMinimumPaymentIsGiven()
+        {
+            Dictionary<Type, int> cost = new() { { typeof(Wood), 2 }, { typeof(Coin), 3 } };
+            Dictionary<Type, int> payment = new() { { typeof(Wood), 2 }, { typeof(Coin), 3 } };
+
+            CostVerifier.VerifyMinimumPayment(cost, payment, out _).Should().Be(true, "payment is the same as cost");
+        }
+
+        [TestMethod]
+        public void PaymentSucceedsIfOverpaymentIsGiven()
+        {
+            Dictionary<Type, int> cost = new() { { typeof(Wood), 2 }, { typeof(Coin), 3 } };
+            Dictionary<Type, int> payment = new() { { typeof(Wood), 3 }, { typeof(Coin), 4 } };
+
+            CostVerifier.VerifyMinimumPayment(cost, payment, out _).Should().Be(true, "payment is the same as cost");
+        }
+
+        [TestMethod]
+        public void VerifyMinimumPaymentMethod_FailsOnLackOfMinimumPayment()
+        {
+            Dictionary<Type, int> cost = new() { { typeof(Wood), 2 }, { typeof(Coin), 3 } };
+            Dictionary<Type, int> payment = new() { { typeof(Wood), 3 }, { typeof(Coin), 2 } };
+
+            CostVerifier.VerifyMinimumPayment(cost, payment, out _).Should().Be(false, "payment is the same as cost");
+        }
+
+        [TestMethod]
+        public void ExactPaymentDoesNotAcceptAdditionalResources_EvenIfUnneeded()
+        {
+            Dictionary<Type, int> cost = new() { { typeof(Wood), 2 }, { typeof(Coin), 3 } };
+            Dictionary<Type, int> payment = new() { { typeof(Wood), 2 }, { typeof(Coin), 3 }, { typeof(Sheep), 4 } };
+
+            CostVerifier.VerifyPaymentExactly(cost, payment).Should().Be(false, "sheep are not needed for the cost");
+        }
+
+        [TestMethod]
+        public void MinimumPaymentAcceptsAdditionalResources_EvenIfUnneeded()
+        {
+            Dictionary<Type, int> cost = new() { { typeof(Wood), 2 }, { typeof(Coin), 3 } };
+            Dictionary<Type, int> payment = new() { { typeof(Wood), 2 }, { typeof(Coin), 3 }, { typeof(Sheep), 4 } };
+
+            CostVerifier.VerifyMinimumPayment(cost, payment, out _).Should().Be(true, "payment is covered even if sheep are not needed for the cost");
+        }
+
+        [TestMethod]
+        public void MinimumPayment_ReturnsCorrectDifferenceForExact()
+        {
+            Dictionary<Type, int> cost = new() { { typeof(Wood), 2 }, { typeof(Coin), 3 } };
+            Dictionary<Type, int> payment = new() { { typeof(Wood), 2 }, { typeof(Coin), 3 } };
+            Dictionary<Type, int> targetOverpayment = new() { { typeof(Wood), 0 }, { typeof(Coin), 0 } };
+
+            CostVerifier.VerifyMinimumPayment(cost, payment, out var overpayment);
+            overpayment.Should().BeEquivalentTo(targetOverpayment, "all resources were paid for exactly");
+        }
+
+        [TestMethod]
+        public void MinimumPayment_ReturnsCorrectDifference()
+        {
+            Dictionary<Type, int> cost = new() { { typeof(Wood), 2 }, { typeof(Coin), 3 } };
+            Dictionary<Type, int> payment = new() { { typeof(Wood), 3 }, { typeof(Coin), 5 } };
+            Dictionary<Type, int> targetOverpayment = new() { { typeof(Wood), 1 }, { typeof(Coin), 2 } };
+
+            CostVerifier.VerifyMinimumPayment(cost, payment, out var overpayment);
+            overpayment.Should().BeEquivalentTo(targetOverpayment, "all resources were paid for exactly");
+        }
+
+        [TestMethod]
+        public void MinimumPayment_ReturnsCorrectDifference_WithExtraResources()
+        {
+            Dictionary<Type, int> cost = new() { { typeof(Wood), 2 }, { typeof(Coin), 3 } };
+            Dictionary<Type, int> payment = new() { { typeof(Wood), 3 }, { typeof(Coin), 5 }, { typeof(Sheep), 3 } };
+            Dictionary<Type, int> targetOverpayment = new() { { typeof(Wood), 1 }, { typeof(Coin), 2 }, { typeof(Sheep), 3 } };
+
+            CostVerifier.VerifyMinimumPayment(cost, payment, out var overpayment);
+            overpayment.Should().BeEquivalentTo(targetOverpayment, "all resources were paid for exactly");
+        }
+
+        // Dummy resources
+        private class Wood
+        {
+            public Wood()
+            {
+            }
+        }
+
+        private class Coin
+        {
+            public Coin()
+            {
+            }
+        }
+
+        private class Sheep
+        {
+            public Sheep()
+            {
+            }
+        }
+    }
+}
---
+++ deckForge/HelperObjects/CostVerifier.cs
@@ -0,0 +1,92 @@
+﻿namespace DeckForge.HelperObjects
+{
+    /// <summary>
+    /// A class that handles verifying that costs are properly paid.
+    /// </summary>
+    public static class CostVerifier
+    {
+        /// <summary>
+        /// Verifies if the passed in cost and paid resources dictionaries are the same.
+        /// </summary>
+        /// <param name="cost">Listed cost of some action or resource.</param>
+        /// <param name="paidResources">Number of resources paid to cover the cost.</param>
+        /// <returns><c>true</c> if dictionaries have the same <see cref="KeyValuePair"/>s, otherwise <c>false</c>.</returns>
+        public static bool VerifyPaymentExactly(Dictionary<Type, int> cost, Dictionary<Type, int> paidResources)
+        {
+            bool equal = false;
+            if (cost.Count == paidResources.Count)
+            {
+                equal = true;
+                foreach (var costPair in cost)
+                {
+                    if (paidResources.TryGetValue(costPair.Key, out int value))
+                    {
+                        if (value != costPair.Value)
+                        {
+                            equal = false;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        equal = false;
+                        break;
+                    }
+                }
+            }
+
+            return equal;
+        }
+
+        /// <summary>
+        /// Verifies if the minimum number of resources were payed, and if they were what the remaining
+        /// resources are.
+        /// </summary>
+        /// <param name="cost">Number of resources that are required to be payed.</param>
+        /// <param name="paidResources">Number of resources that were paid to cover the cost.</param>
+        /// <param name="overpayedResources">Difference in resources if payment was succesful. Otherwise output is <c>null</c>
+        /// if payment failed.</param>
+        /// <returns><c>true</c> if payment covered the cost, otherwise <c>false</c>. <paramref name="overpayedResources"/> will
+        /// show the difference in paid resources if <c>true</c>, otherwise will be null.</returns>
+        public static bool VerifyMinimumPayment(
+            Dictionary<Type, int> cost,
+            Dictionary<Type, int> paidResources,
+            out Dictionary<Type, int>? overpayedResources)
+        {
+            overpayedResources = paidResources;
+
+            bool payed = false;
+            if (paidResources.Count >= cost.Count)
+            {
+                payed = true;
+                foreach (var costPair in cost)
+                {
+                    if (paidResources.TryGetValue(costPair.Key, out int paidValue))
+                    {
+                        if (paidValue < costPair.Value)
+                        {
+                            payed = false;
+                            break;
+                        }
+                        else
+                        {
+                            overpayedResources[costPair.Key] = paidValue - costPair.Value;
+                        }
+                    }
+                    else
+                    {
+                        payed = false;
+                        break;
+                    }
+                }
+            }
+
+            if (!payed)
+            {
+                overpayedResources = null;
+            }
+
+            return payed;
+        }
+    }
+}
