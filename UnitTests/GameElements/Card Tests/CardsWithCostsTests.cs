2eaf00fe6b898ee83d63d9b3ed8d3c05449b0369
Joshua Jacobs <joshua.jacobs126@gmail.com>
2022-11-28 10:03:44 -0600

    Update CardsWithCostsTests.cs

--- UnitTests/GameElements/Card Tests/CardsWithCostsTests.cs
+++ UnitTests/GameElements/Card Tests/CardsWithCostsTests.cs
@@ -2,6 +2,7 @@
 using DeckForge.GameElements.Resources;
 using DeckForge.GameElements.Resources.Cards;
 using DeckForge.GameElements.Resources.Cards.Example_Cards;
+using DeckForge.PlayerConstruction;
 using FluentAssertions;
 
 namespace UnitTests.GameElements.Card_Tests
@@ -28,6 +29,35 @@ namespace UnitTests.GameElements.Card_Tests
             Dictionary<Type, int> payment = new() { { typeof(Mana), manaCount } };
 
             card.PayCostExactly(payment).Should().BeOfType<NullCard>("the incorrect payment count is given");
+        }
+
+        [TestMethod]
+        public void CardGainedEvenWithOverpayment_IfSpecified()
+        {
+            ICardWithCost card = new BarBrawlerCharacterCardWithCost(new BaseGameMediator(0)); // Needs 2 mana.
+            Dictionary<Type, int> payment = new() { { typeof(Mana), 2 }, { typeof(BasePlayer), 1 } };
+
+            card.PayCostAndGetRemainingResources(payment, out var _).Should().BeEquivalentTo(card, "card is gained because minimum payment is provided");
+        }
+
+        [TestMethod]
+        public void RemainderGivenWithOverpayment_IfSpecified()
+        {
+            ICardWithCost card = new BarBrawlerCharacterCardWithCost(new BaseGameMediator(0));
+            Dictionary<Type, int> payment = new() { { typeof(Mana), 2 }, { typeof(BasePlayer), 1 } };
+            Dictionary<Type, int> remainderValidation = new() { { typeof(Mana), 0 }, { typeof(BasePlayer), 1 } };
+
+            card.PayCostAndGetRemainingResources(payment, out var remainder);
+            remainder.Should().BeEquivalentTo(remainderValidation, "these are the remaining resources left");
+        }
+
+        [TestMethod]
+        public void NullCardGainedWithInsufficientResources_EvenWithOverpaymentMethod()
+        {
+            ICardWithCost card = new BarBrawlerCharacterCardWithCost(new BaseGameMediator(0)); // Needs 2 mana.
+            Dictionary<Type, int> payment = new() { { typeof(Mana), 1 }, { typeof(BasePlayer), 1 } };
+
+            card.PayCostAndGetRemainingResources(payment, out var _).Should().BeOfType<NullCard>("the incorrect minimum payment count is given");
         }
     }
 }
