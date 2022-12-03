2b4a09eae49d5d75b0564a9202db5495f21c05d4
Joshua Jacobs <joshua.jacobs126@gmail.com>
2022-12-03 15:40:48 -0600

    Market area lists the decks in area

---
+++ UnitTests/DominionTests/MarketTableAreaTests.cs
@@ -0,0 +1,46 @@
+﻿using DeckForge.GameConstruction.PresetGames.Dominion.Cards;
+using DeckForge.GameConstruction.PresetGames.Dominion.Table;
+using DeckForge.GameElements.Resources;
+using DeckForge.HelperObjects;
+using DeckForge.PlayerConstruction;
+using FluentAssertions;
+using UnitTests.Mocks;
+
+namespace UnitTests.DominionTests
+{
+    [TestClass]
+    public class MarketTableAreaTests
+    {
+        [TestMethod]
+        public void DecksAreDisplayedCorrectlyThroughPrompt()
+        {
+            ConsoleOutputMock output = new();
+            List<IDeck> decks = new()
+            {
+                new MonotoneDeck(typeof(CopperCard), 6),
+                new MonotoneDeck(typeof(SilverCard), 4),
+                new MonotoneDeck(typeof(GoldCard), 3)
+            };
+            DominionMarketTableArea market = new(decks);
+
+            List<string> marketList = market.GetMarketAreaAsStringList();
+            Dictionary<int, string> prompt = new() { { 0, "Market:" } };
+            for (int i = 0; i < marketList.Count; i++)
+            {
+                prompt.Add(i + 1, marketList[i]);
+            }
+
+            PlayerPrompter pp = new(new ConsoleInputMock(new() { "1" }), output, prompt);
+            pp.Prompt();
+
+            List<string> expectedOutput = new()
+            {
+                "Market:",
+                "\t1) Copper - [6 Remaining]",
+                "\t2) Silver - [4 Remaining]",
+                "\t3) Gold - [3 Remaining]"
+            };
+            output.CompleteOutput.Should().BeEquivalentTo(expectedOutput);
+        }
+    }
+}
--- deckForge/GameConstruction/PresetGames/Dominion/Table/DominionMarketTableArea.cs
+++ deckForge/GameConstruction/PresetGames/Dominion/Table/DominionMarketTableArea.cs
@@ -1,4 +1,6 @@
-﻿using DeckForge.GameElements.Table;
+﻿using DeckForge.GameConstruction.PresetGames.Dominion.Cards;
+using DeckForge.GameElements.Resources;
+using DeckForge.GameElements.Table;
 
 namespace DeckForge.GameConstruction.PresetGames.Dominion.Table
 {
@@ -10,9 +12,55 @@ namespace DeckForge.GameConstruction.PresetGames.Dominion.Table
         /// <summary>
         /// Initializes a new instance of the <see cref="DominionMarketTableArea"/> class.
         /// </summary>
-        public DominionMarketTableArea()
-            : base(id: 0, TablePlacementZoneType.NeutralZone, areaCardLimit: 0)
+        /// <param name="decks">The list of <see cref="IDeck"/>s that can be bought from in this <see cref="TableArea"/>.</param>
+        public DominionMarketTableArea(List<IDeck> decks)
+            : base(id: 0, TablePlacementZoneType.NeutralZone, decks, areaCardLimit: 0)
         {
+        }
+
+        public List<string> GetMarketAreaAsStringList()
+        {
+            List<string> market = new();
+
+            for (int i = 0; i < Decks.Count; i++)
+            {
+                string topCard = GetStringOfTopCardIfNotEmptyDeck(Decks[i]);
+                string deckString = WriteStringForDeck(Decks[i], topCard);
+                AddToMarketListIfDeckStringNotEmpty(market, deckString);
+            }
+
+            return market;
+        }
+
+        private string GetStringOfTopCardIfNotEmptyDeck(IDeck deck)
+        {
+            string cardRepresentation = string.Empty;
+
+            if (deck.Deck.Count is not 0)
+            {
+                cardRepresentation = ((DominionCard)deck.Deck[0]).Name;
+            }
+
+            return cardRepresentation;
+        }
+
+        private string WriteStringForDeck(IDeck deck, string cardString)
+        {
+            string output = string.Empty;
+            if (cardString is not "")
+            {
+                output = cardString + $" - [{deck.Count} Remaining]";
+            }
+
+            return output;
+        }
+
+        private void AddToMarketListIfDeckStringNotEmpty(List<string> market, string deckString)
+        {
+            if (deckString is not "")
+            {
+                market.Add(deckString);
+            }
         }
     }
 }
