ce7c9aeea186e6079574fb6ca55990826c093d8d
Joshua Jacobs <joshua.jacobs126@gmail.com>
2022-12-02 21:09:30 -0600

    Create NullDeck.cs

---
+++ deckForge/GameElements/Resources/Cards/NullDeck.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+
+namespace DeckForge.GameElements.Resources.Cards
+{
+    /// <summary>
+    /// A <see cref="IDeck"/> of nothing. Gives <see cref="NullCard"/>s if drawn from.
+    /// </summary>
+    public class NullDeck : IDeck
+    {
+        /// <inheritdoc/>
+        public Type ResourceType
+        {
+            get { return typeof(NullCard); }
+        }
+
+        /// <inheritdoc/>
+        public int Count
+        {
+            get { return 0; }
+        }
+
+        /// <inheritdoc/>
+        public void AddCardToDeck(ICard card, string pos = "bottom", bool shuffleAfter = false)
+        {
+        }
+
+        /// <inheritdoc/>
+        public void AddMultipleCardsToDeck(List<ICard> cards, string pos = "bottom", bool shuffleAfter = false)
+        {
+        }
+
+        /// <inheritdoc/>
+        public void AddMultipleResources(IList resource)
+        {
+        }
+
+        /// <inheritdoc/>
+        public void AddResource(ICard resource)
+        {
+        }
+
+        /// <inheritdoc/>
+        public List<ICard>? ClearCollection()
+        {
+            return null;
+        }
+
+        /// <inheritdoc/>
+        public void DecrementResourceCollection()
+        {
+        }
+
+        /// <inheritdoc/>
+        public ICard? DrawCard(bool drawFacedown = false)
+        {
+            return new NullCard();
+        }
+
+        public List<ICard?> DrawMultipleCards(int count)
+        {
+            return new List<ICard?>() { new NullCard() };
+        }
+
+        /// <inheritdoc/>
+        public ICard? GainResource()
+        {
+            return new NullCard();
+        }
+
+        /// <inheritdoc/>
+        public void IncrementResourceCollection()
+        {
+        }
+
+        /// <inheritdoc/>
+        public void RemoveResource(ICard resource)
+        {
+        }
+
+        /// <inheritdoc/>
+        public void Shuffle()
+        {
+        }
+    }
+}
