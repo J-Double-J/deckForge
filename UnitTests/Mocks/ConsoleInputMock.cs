7d845839c0075f8db5f162bbfe462154612581bd
Joshua Jacobs <joshua.jacobs126@gmail.com>
2022-12-02 21:51:06 -0600

    Console Mocks were made

---
+++ UnitTests/Mocks/ConsoleInputMock.cs
@@ -0,0 +1,30 @@
+﻿using DeckForge.HelperObjects;
+
+namespace UnitTests.Mocks
+{
+    /// <summary>
+    /// A console mock that gives an input of strings that are iterated over after each call of <see cref="GetInput"/>.
+    /// </summary>
+    public class ConsoleInputMock : IInputReader
+    {
+        private readonly List<string> input;
+        private int nextInput = 0;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConsoleInputMock"/> class.
+        /// </summary>
+        /// <param name="input">List of strings that represent input from a user source.</param>
+        public ConsoleInputMock(List<string> input)
+        {
+            this.input = input;
+        }
+
+        /// <inheritdoc/>
+        public string? GetInput()
+        {
+            var inputString = input[nextInput];
+            nextInput++;
+            return inputString;
+        }
+    }
+}
---
+++ UnitTests/Mocks/ConsoleOutputMock.cs
@@ -0,0 +1,35 @@
+﻿using DeckForge.HelperObjects;
+
+namespace UnitTests.Mocks
+{
+    /// <summary>
+    /// Mocks the output from a console. Tracks the currently displayed string, as well as all the outputs displayed to this mock.
+    /// </summary>
+    public class ConsoleOutputMock : IOutputDisplay
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConsoleOutputMock"/> class.
+        /// </summary>
+        public ConsoleOutputMock()
+        {
+            CompleteOutput = new();
+        }
+
+        /// <summary>
+        /// Gets the currently "displayed" output.
+        /// </summary>
+        public string? CurrentOutput { get; private set; }
+
+        /// <summary>
+        /// Gets the list of all outputs that were displayed.
+        /// </summary>
+        public List<string> CompleteOutput { get; }
+
+        /// <inheritdoc/>
+        public void Display(string output)
+        {
+            CurrentOutput = output;
+            CompleteOutput.Add(output);
+        }
+    }
+}
