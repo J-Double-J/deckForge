95a0bd8595d18615cff83b431079e7e791d3a5ea
Joshua Jacobs <joshua.jacobs126@gmail.com>
2022-12-03 13:51:58 -0600

    Prompter now is more abstracted and can "cancel"

--- UnitTests/PlayerConstruction/PlayerPrompterTests.cs
+++ UnitTests/PlayerConstruction/PlayerPrompterTests.cs
@@ -1,5 +1,6 @@
 ﻿using DeckForge.PlayerConstruction;
 using FluentAssertions;
+using UnitTests.Mocks;
 
 namespace UnitTests.PlayerConstruction
 {
@@ -21,6 +22,61 @@ namespace UnitTests.PlayerConstruction
             PlayerPrompter pp = new(prompt);
 
             pp.Prompt().Should().Be(1, "the Player chose option 1.");
+        }
+
+        [TestMethod]
+        public void PrompterAllowsBack_MarkedWithNegativeNumber()
+        {
+            Dictionary<int, string> prompt = new()
+            {
+                { 0, "How do we think this test will go?" },
+                { 1, "Good" },
+                { 2, "Bad" },
+                { -1, "Cancelled" }
+            };
+            PlayerPrompter pp = new(new ConsoleInputMock(new() { "-1" }), new ConsoleOutputMock(), prompt, true);
+
+            pp.Prompt().Should().Be(-1, "the user elected to cancel the prompt");
+        }
+
+        [TestMethod]
+        public void PrompterDoesNotAllowBadResponses()
+        {
+            Dictionary<int, string> prompt = new()
+            {
+                { 0, "How do we think this test will go?" },
+                { 1, "Good" },
+                { 2, "Bad" },
+                { -1, "Cancelled" }
+            };
+            PlayerPrompter pp = new(new ConsoleInputMock(new() { "-2", "0", "99", "1" }), new ConsoleOutputMock(), prompt, true);
+
+            pp.Prompt().Should().Be(1, "the user tried multiple bad responses before choosing the correct one");
+        }
+
+        [TestMethod]
+        public void PrompterDisplaysOutputCorrectly()
+        {
+            ConsoleOutputMock consoleOutput = new();
+            Dictionary<int, string> prompt = new()
+            {
+                { 0, "How do we think this test will go?" },
+                { 1, "Good" },
+                { 2, "Bad" },
+                { -1, "Cancelled" }
+            };
+            PlayerPrompter pp = new(new ConsoleInputMock(new() { "1" }), consoleOutput, prompt, true);
+            List<string> expectedOutput = new()
+            {
+                "How do we think this test will go?",
+                "\t1) Good",
+                "\t2) Bad",
+                "\t-1) Cancelled"
+            };
+
+            pp.Prompt();
+
+            consoleOutput.CompleteOutput.Should().BeEquivalentTo(expectedOutput);
         }
     }
 }
--- deckForge/PlayerConstruction/PlayerPrompter.cs
+++ deckForge/PlayerConstruction/PlayerPrompter.cs
@@ -1,5 +1,11 @@
-﻿namespace DeckForge.PlayerConstruction
+﻿using DeckForge.HelperObjects;
+using System.Security.Cryptography.X509Certificates;
+
+namespace DeckForge.PlayerConstruction
 {
+    /// <summary>
+    /// Prompts the <see cref="IPlayer"/> for some input and returns a validated input response.
+    /// </summary>
     public class PlayerPrompter
     {
         private Dictionary<int, string> prompt;
@@ -8,12 +14,51 @@
         /// Initializes a new instance of the <see cref="PlayerPrompter"/> class.
         /// </summary>
         /// <param name="prompt">The prompt the player will be shown when they need to make a choice. First option
-        /// starts at 1. For any headers in the prompt set it to key 0.</param>
+        /// starts at 1. For any headers in the prompt set it to key 0. Prompt is displayed in order of the dictionary.</param>
         /// <param name="optionCount">The number of valid options for the <see cref="IPlayer"/> to choose from.</param>
         public PlayerPrompter(Dictionary<int, string> prompt)
+            : this(prompt, false)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PlayerPrompter"/> class. Allows going back and cancelling.
+        /// </summary>
+        /// <param name="prompt">The prompt the player will be shown when they need to make a choice. First option
+        /// starts at 1. For any headers in the prompt set it to key 0. For any cancel text set it to key -1. Prompt is displayed in order of
+        /// the dictionary.</param>
+        /// /// <param name="cancelAllowed">Sets whether or not this prompt will allow a user to cancel. Default is <c>false</c>.</param>
+        public PlayerPrompter(Dictionary<int, string> prompt, bool cancelAllowed)
+            : this(new ConsoleReader(), new ConsoleOutput(), prompt, cancelAllowed)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PlayerPrompter"/> class. Can specify whether cancelling is allowed, as well
+        /// as input and output.
+        /// </summary>
+        /// <param name="reader">Specifies where to get user input.</param>
+        /// <param name="output">Specifies where to display any output.</param>
+        /// <param name="prompt">The prompt the player will be shown when they need to make a choice. First option
+        /// starts at 1. For any headers in the prompt set it to key 0. For any cancel text set it to key -1. Prompt is displayed in order of
+        /// the dictionary.</param>
+        /// /// <param name="cancelAllowed">Sets whether or not this prompt will allow a user to cancel. Default is <c>false</c>.</param>
+        public PlayerPrompter(IInputReader reader, IOutputDisplay output, Dictionary<int, string> prompt, bool cancelAllowed = false)
         {
             this.prompt = prompt;
+            CancelAllowed = cancelAllowed;
+            InputReader = reader;
+            OutputDisplay = output;
         }
+
+        /// <summary>
+        /// Gets a value indicating whether this prompt allows a user to cancel.
+        /// </summary>
+        public bool CancelAllowed { get; }
+
+        private IInputReader InputReader { get; }
+
+        private IOutputDisplay OutputDisplay { get; }
 
         /// <summary>
         /// Presents the Player with the given prompt.
@@ -24,42 +69,61 @@
             string? response;
             do
             {
-                foreach (var entry in prompt)
-                {
-                    if (entry.Key != 0)
-                    {
-                        Console.WriteLine($"\t{entry.Key}) {entry.Value}");
-                    }
-                    else
-                    {
-                        Console.WriteLine(entry.Value);
-                    }
-                }
-
-                response = Console.ReadLine();
+                DisplayPrompt();
+                response = InputReader.GetInput();
             }
             while (!IsValidResponse(response));
 
             return int.Parse(response!);
         }
 
+        private void DisplayPrompt()
+        {
+            foreach (var entry in prompt)
+            {
+                OutputPromptEntry(entry);
+            }
+        }
+
+        private void OutputPromptEntry(KeyValuePair<int, string> entry)
+        {
+            if (entry.Key != 0)
+            {
+                OutputDisplay.Display($"\t{entry.Key}) {entry.Value}");
+            }
+            else
+            {
+                OutputDisplay.Display(entry.Value);
+            }
+        }
+
         private bool IsValidResponse(string? response)
         {
             if (int.TryParse(response, out int numericResponse))
             {
-                if (numericResponse > 0 && prompt.ContainsKey(numericResponse))
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
+                return ValidateWhetherIntegerIsInBounds(numericResponse);
             }
             else
             {
                 return false;
             }
+        }
+
+        private bool ValidateWhetherIntegerIsInBounds(int response)
+        {
+            if (response > 0 && prompt.ContainsKey(response))
+            {
+                return true;
+            }
+            else if (response == -1 && CancelAllowed && prompt.ContainsKey(-1))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
         }
     }
 }
